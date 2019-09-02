cd489fd83336c93f1afb9b0e9ae1e73769ec2158
许丹琦 <1204253728@qq.com>
2019-09-02 20:33:52 +0800

    获取仓库

--- src/create.js
+++ src/create.js
@@ -1,4 +1,20 @@
 // create 所有的逻辑
-module.exports = (projectName) => {
-  console.log('projectName', projectName);
+
+// https://api.github.com/users/goofyrepo/repos 仓库地址
+
+const axios = require('axios');
+
+// 1)  获取仓库列表
+const fetchRepoList = async () => {
+  const { data } = await axios.get('https://api.github.com/users/goofyrepo/repos');
+  return data;
+};
+
+module.exports = async (projectName) => {
+  // 1)  获取项目模版
+  let repos = await fetchRepoList();
+  repos = repos.map((item) => item.name);
+  console.log(repos);
+
+  // loading
 };
