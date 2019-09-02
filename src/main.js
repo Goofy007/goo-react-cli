46dd521cabab7aade7566fd6c1513fb7ef2d767f
许丹琦 <1204253728@qq.com>
2019-09-02 20:01:14 +0800

    配置commander命令

--- src/main.js
+++ src/main.js
@@ -1,1 +1,44 @@
-console.log("ok")
+
+// 1) 解析用户的参数
+const program = require('commander');
+
+const mapActions = {
+  create: {
+    alias: 'c',
+    description: 'create a project',
+    examples: [
+      'goo-react-cli create <projct-name>',
+    ],
+  },
+  '*': {
+    alias: '',
+    description: 'command not found',
+    examples: [],
+  },
+};
+
+Reflect.ownKeys(mapActions).forEach((action) => {
+  program
+    .command(action)
+    .alias(mapActions[action].alias)
+    .description(mapActions[action].description)
+    .action(() => {
+      if (action === '*') {
+        console.log(mapActions[action].description);
+      } else {
+        console.log(action);
+      }
+    });
+});
+
+// help
+program.on('--help', () => {
+  console.log('\nExamples:');
+  Reflect.ownKeys(mapActions).forEach((action) => {
+    mapActions[action].examples.forEach((example) => {
+      console.log(example);
+    });
+  });
+});
+
+program.version('1.0.0').parse(process.argv);
